0aaff3eb350ed45d4f2f4baefa399f73678d708a
tienquan1999 <zed.quan99@gmail.com>
2019-11-11 15:36:36 +0700

    api get item and get category in mobile app

---
+++ controller/MobileApp/item.controller.js
@@ -0,0 +1,72 @@
+const db = require("../../models/index");
+const response = require("../../util/response");
+const createUrl = require("../../util/s3/createUrl");
+
+async function getCategoryInRestaurant(req, res){
+    let {id_restaurant} = req.params;
+    try{
+        let employees = await db.Employees.findOne({
+            where: {
+                id_employees: req.tokenData.id_employees,
+                id_restaurant: id_restaurant
+            }
+        });
+        if(!employees){
+            throw new Error("Bạn không phải nhân viên của cửa hàng này.")
+        }
+        let categories = await db.Category.findAll({
+            where: {
+                id_restaurant: id_restaurant
+            }
+        });
+        return res.json(response.buildSuccess({categories}));
+    }
+    catch(err){
+        console.log("getCategoryInRestaurant: ", err.message);
+        return res.json(response.buildFail(err))
+    }
+}
+
+async function getItemInCategory(req, res){
+    try{
+        let {id_restaurant} = req.params;
+        let {id_category} = req.query;
+        let constrains = {
+            id_restaurant: id_restaurant
+        };
+        if(id_category){
+            constrains.id_category = id_category
+        }
+        let employees = await db.Employees.findOne({
+            where: {
+                id_employees: req.tokenData.id_employees,
+                id_restaurant: id_restaurant
+            }
+        });
+        if(!employees){
+            throw new Error("Bạn không phải là nhân viên của nhà hàng này.")
+        }
+        let items = await Item.findAll({
+            where: constrains,
+            order: [
+                ["price", 'ASC']
+            ]
+        });
+        items = items.map(e => e.dataValues);
+        for(let e of items){
+            e.image = await createUrl(e.image)
+        }
+        res.json(response.buildSuccess({items}))
+    }
+    catch(err){
+        console.log("getItems: ", err.message );
+        return res.json(response.buildFail(err.message))
+    }
+}
+
+
+
+module.exports = {
+    getCategoryInRestaurant,
+    getItemInCategory
+};
--- router/MobileApp.js
+++ router/MobileApp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Employees = require("../controller/MobileApp/employees.controller");
+const Item = require("../controller/MobileApp/item.controller");
 const VerifyEmployees = require("../middleware/verifyEmployees");
 
 router.get("/ping", (req, res) => {
@@ -8,6 +9,10 @@ router.get("/ping", (req, res) => {
 });
 router.get("/restaurants/:id_restaurant/items", VerifyEmployees, Employees.getItems);
 router.get("/restaurants/:id_restaurant/tables", VerifyEmployees, Employees.getTables);
+
+router.get("/restaurants/:id_restaurant/categories", VerifyEmployees, Item.getCategoryInRestaurant);
+router.get("/restaurants/:id_restaurant/items", VerifyEmployees, Item.getItemInCategory);
+
 router.post("/login", Employees.login);
 
 
